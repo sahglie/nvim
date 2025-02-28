5fd670cfe046916ea9904aa5e96c61516927b593
runner <runner@berkeley.edu>
2025-02-27 16:14:49 -0800

    including .gitignored files in default search is too slow, create separate command for searching through ignored files for perf

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -29,7 +29,7 @@ return {
       pickers = {
         find_files = {
           hidden = true,
-          no_ignore = true
+          no_ignore = false
         }
       },
       extensions = {
@@ -44,6 +44,9 @@ return {
     vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files)
     vim.keymap.set("n", "<leader>b", require("telescope.builtin").buffers)
     vim.keymap.set("n", "<leader>f", require("telescope.builtin").find_files)
+    vim.keymap.set("n", "<leader>fi", function()
+      require("telescope.builtin").find_files({ hidden = true, no_ignore = true })
+    end)
     vim.keymap.set("n", "<leader>sk", require("telescope.builtin").keymaps)
     vim.keymap.set("n", "<leader>sa", require("telescope.builtin").commands)
     vim.keymap.set("n", "<leader>G", require("telescope.builtin").commands)
