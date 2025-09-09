25630a026cb5296a67dc857c28dc90eaaabe07c2
runner <runner@berkeley.edu>
2025-09-09 10:31:27 -0700

    add obsidian to new config, disable marksman for now

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -77,6 +77,7 @@ return {
           },
         },
         educationalsp = {},
+        marksman = false,
       },
     },
   },
---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,21 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",
+  lazy = true,
+  ft = "markdown",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "sahglie-vault",
+        path = os.getenv("HOME") .. "/Development/projects/obsidian-vault",
+      },
+    },
+    daily_notes = {
+      folder = "UCB/TODOs",
+      date_format = "%Y-%m-%d",
+    },
+  },
+}
