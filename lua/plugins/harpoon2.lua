b44cbf7e0a16279a9cd3392d67e8535d91606f55
runner <runner@berkeley.edu>
2025-12-20 10:26:45 -0800

    update harpoon keybinding

--- lua/plugins/harpoon2.lua
+++ lua/plugins/harpoon2.lua
@@ -3,14 +3,14 @@ return {
   keys = {
     -- Keep the ones you want
     {
-      "<leader>H",
+      "<leader>Ha",
       function()
         require("harpoon"):list():add()
       end,
       desc = "Harpoon File",
     },
     {
-      "<leader>h",
+      "<leader>Hq",
       function()
         local harpoon = require("harpoon")
         harpoon.ui:toggle_quick_menu(harpoon:list())
