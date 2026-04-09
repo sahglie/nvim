aa3970a1c42b7e09cd8111bfcae9c6eef0590eff
runner <runner@berkeley.edu>
2026-04-09 09:49:30 -0700

    disable nvim-lint for go, I don't want to see linting complaints in real time, way overkill

---
+++ lua/plugins/nvim-lint.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    opts = function(_, opts)
+      opts.linters_by_ft = opts.linters_by_ft or {}
+      opts.linters_by_ft.go = {} -- Disable golangci-lint for Go
+      return opts
+    end,
+  },
+}
